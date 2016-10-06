921c9cae118f26e0788768072f2e5d92f8ed005e
Foad R ShariatPanahi <jesuschrist.love@hotmail.com>
2016-10-05 21:36:16 -0700

    Revert "Revert "Add project files.""
    This reverts commit 35e09e4606c3d9c9988c4f1b57d6831b57e16ebc.

---
+++ IranianParastesh/Controllers/Api/SongsController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IranianParastesh.Controllers.Api
+{
+    public class SongsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public SongsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        //GET: /api/songs
+        [HttpGet]
+        public IHttpActionResult GetSongs()
+        {
+            var songs = _context.Songs.ToList();
+
+            return Ok(songs);
+        }
+
+        //GET: /api/songs/1
+        [HttpGet]
+        public IHttpActionResult GetSong(int id)
+        {
+            var song = _context.Songs.SingleOrDefault(s => s.Id == id);
+
+            if (song == null)
+                return BadRequest();
+
+            return Ok(song);
+        }
+    }
+}
